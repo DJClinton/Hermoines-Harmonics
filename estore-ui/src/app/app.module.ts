ce01b5926b00ebf356b5f7a0af5edbb0cee8756b
Hiro T <hiroto125takeuchi@gmail.com>
2023-06-18 22:12:26 -0400

    feat(feature): add searchbox (#3)
    * feat(feature): add searchbox
    
    * fix(feature): angular material typography
    
    * fix(feature): fix backend url change

--- estore-ui/src/app/app.module.ts
+++ estore-ui/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductDetailComponent } from './screen/product-detail/product-detail.component';
+import { SearchBarComponent } from './component/search-bar/search-bar.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchBarComponent, ProductDetailComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ estore-ui/src/app/product.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  private inverntoryUrl = 'http://localhost:8080/inventory';
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.inverntoryUrl);
+  }
+
+  getProduct(id: number): Observable<any> {
+    const url = `${this.inverntoryUrl}/${id}`;
+    return this.http
+      .get(url)
+      .pipe(catchError(this.handleError<any>(`getProduct id:${id}`)));
+  }
+
+  addProduct(product: any): Observable<any> {
+    return this.http
+      .post(this.inverntoryUrl, product, this.httpOptions)
+      .pipe(catchError(this.handleError<any>('addProduct')));
+  }
+
+  updateProduct(product: any): Observable<any> {
+    return this.http
+      .put(this.inverntoryUrl, product, this.httpOptions)
+      .pipe(catchError(this.handleError<any>('updateProduct')));
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    const url = `${this.inverntoryUrl}/${id}`;
+    return this.http
+      .delete(url, this.httpOptions)
+      .pipe(catchError(this.handleError<any>('deleteProduct')));
+  }
+
+  searchProductsByName(term: string): Observable<any> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http
+      .get(`${this.inverntoryUrl}/?name=${term}`)
+      .pipe(
+        catchError(this.handleError<any>(`searchProducts term:${term}`, []))
+      );
+  }
+}
