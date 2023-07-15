8bd398863608a5f61989b0d3abf454af1bfd120e
Chadbino <djf7085@rit.edu>
2023-07-14 20:06:00 -0400

    Finished logic for editing list of products

--- estore-ui/src/app/products/products.component.ts
+++ estore-ui/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service'
 import { Product } from '../product';
 
 @Component({
@@ -7,10 +8,37 @@ import { Product } from '../product';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-  product: Product = {
-    id: 1,
-    name: 'Tuba',
-    price: 599.99,
-    quantity: 5
-  };
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    });
+  }
+
+  addProduct(name: string, price: number, quantity: number) {
+    const id: number = 0; // Server will handle the actual product ID, replacing this temporary value.
+    const newProduct: Product = {id, name, price, quantity};
+    this.productService.addProduct(newProduct).subscribe(() => {
+      this.getProducts(); // Refreshes product list
+    });
+  }
+
+  updateProduct(product: Product) {
+    this.productService.updateProduct(product).subscribe(() => {
+      this.getProducts();
+    });
+  }
+
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(id).subscribe(() => {
+      this.getProducts();
+    });
+  }
 }
