fe6b52be64983c5b167605484b2c88b72a950187
Chadbino <djf7085@rit.edu>
2023-07-17 00:09:18 -0400

    Fixed routing module issues

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailComponent } from './screen/product-detail/product-detail.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: 'product/:id', component: ProductDetailComponent },
+  { path: '', redirectTo: '/inventory', pathMatch: 'full'},
+  { path: '/inventory', component: ProductsComponent}
 ];
 
 @NgModule({
