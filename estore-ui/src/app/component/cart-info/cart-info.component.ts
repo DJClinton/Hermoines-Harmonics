61ad725bd77abbaa9a93a591675aa4226f77e766
caity k <cdk6546@rit.edu>
2023-07-30 00:59:50 -0400

    added buyer id to page

--- estore-ui/src/app/component/cart-info/cart-info.component.ts
+++ estore-ui/src/app/component/cart-info/cart-info.component.ts
@@ -1,8 +1,6 @@
-import { Token } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { BuyerInfoService } from 'src/app/service/buyerInfo.service';
-import { AuthorizationService } from 'src/app/service/authorization.service';
-import { CartServiceService } from 'src/app/service/cart-service.service';
+
 
 @Component({
   selector: 'app-cart-info',
@@ -14,6 +12,7 @@ export class CartInfoComponent {
   userID : any
   user: any
   buyerInfo: any
+  buyerID: any
   constructor(private infoService: BuyerInfoService){}
 
   ngOnInit(): void {
@@ -28,6 +27,7 @@ export class CartInfoComponent {
         (userData: any) => {
           this.user = userData;
           this.buyerInfo = userData;
+          this.buyerID = this.buyerInfo.id;
           this.getCart();
         },
         (error) => {
